0a435a9ce8b0ddd24e8d68fbf0e051436212e42c
Gabe-Lau <20laugabriel@bprep.org>
2018-09-07 09:17:27 -0700

    Unit 1 Lab ez

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,19 @@
+#myName=input()
+#print(myName)
+
+#print("my name is " + myName)
+#answer = (61253172 * 6438268747658735748)
+#print (answer)
+
+#num1=input()
+#print (num1)
+#num2=input()
+#print (num2)
+
+#print (num1 + num2)
+#print (int(num1) + int(num2))
+
+intA = 25
+intB = 25
+intC = 35
+print (intA != intB)
