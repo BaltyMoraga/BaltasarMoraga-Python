1dc6c95988ad33bf43cf356a1d723ee291a11be3
Gabe-Lau <20laugabriel@bprep.org>
2018-09-11 09:18:17 -0700

    skr

---
+++ Unit2Blab/Unit2Blab.py
@@ -0,0 +1,20 @@
+numGrade = int(input())
+print (numGrade)
+if numGrade >= 90:
+    print("Yahoo you got an A")
+
+elif numGrade >= 80:
+    print("Good you got a B")
+
+elif numGrade >= 70:
+    print("Okay you got a C")
+
+else:
+    print("You better study harder")
+
+
+userlnut = int(input())
+print (userlnut)
+if userlnut == blue:
+    print (The sky is blue)
+elif userlnut == Green:
