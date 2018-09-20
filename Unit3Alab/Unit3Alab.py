46738b880667efa2d032702c4276b2938dfec7eb
Gabe-Lau <20laugabriel@bprep.org>
2018-09-20 09:23:58 -0700

    good

--- Unit3Alab/Unit3Alab.py
+++ Unit3Alab/Unit3Alab.py
@@ -1,40 +1,51 @@
-def myFunc():
-    print('sus')
-    print('sauce')
-    print('you are fine')
+#def myFunc():
+    #print('sus')
+    #print('sauce')
+    #print('you are fine')
 
-print('starts here')
-myFunc()
-
-
-def howManyDaysOld():
-    age = input('how old are you?')
-    daysOld = (int(age) * 365)
-    print('you are ' + str(daysOld) + ' days old')
-
-print('starts here')
-howManyDaysOld()
+#print('starts here')
+#myFunc()
 
 
-def homeBase():
-    home = input('what city are you from? - ')
-    schoolYears = input('how many years have you been in school? - ')
-    print('you are from ' + str(home))
-    print('you have been in school for ' + str(schoolYears) + ' years')
+#def howManyYearsAtSchool():
+    #myGrade = input('what grade are you in at school? - ')
+    #print('you have been in school for ' + str(myGrade) + ' years')
 
-print('starst here')
-homeBase()
+#howManyYearsAtSchool()
+
+
+#def homeBase():
+    #home = input('what city are you from? - ')
+    #schoolYears = input('how many years have you been in school? - ')
+    #print('you are from - ' + str(home))
+    #print('you are in grade - ' + str(schoolYears))
+
+#print('starts here')
+#homeBase()
 
 
 from random import *
 
-def randomNumber():
-    x = input('what is my start value?')
-    y = input('what is my end value')
-    myNumber = randint(int(x), int(y))
-    print(str(myNumber) + ' is a random number between ')
-    print(str(x) + ' and ' + str(y))
+#def randomNumber():
+    #x = input('what is my start value? - ')
+    #3y = input('what is my end value? - ')
+    #myNumber = randint(int(x), int(y))
+    #print(str(myNumber) + ' is a random number between - ')
+    #print(str(x) + ' and ' + str(y))
 
-print('starts here')
-randomNumber()
+#print('starts here')
+#randomNumber()
 
+
+
+def boxArea(L, W):
+    area = int(L) * int(W)
+    perimeter = int(L) *2 + int(W) *2
+    print('perimeter of the box is ' + str(perimeter))
+    print('area of the box is ' + str(area))
+
+L = input('what is the length of the box? - ')
+w = input('what is the width of the box? - ')
+boxArea (L, W)
+
+boxArea(length, width)
