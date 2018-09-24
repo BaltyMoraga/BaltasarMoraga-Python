3213284e815e05f27ebfbd16dc66fd5cb63ab07a
Gabe-Lau <20laugabriel@bprep.org>
2018-09-24 08:50:05 -0700

    lab321

---
+++ Lab321/Lab321.py
@@ -0,0 +1,16 @@
+def main():
+    yearInSchool = (9,10,11,12)
+    print(yearInSchool)
+    input('What year in school are you in - ')
+    if input(9):
+        print('freshman')
+    elif input(10):
+        print('sophmore')
+    elif input(11):
+        print('junior')
+    elif input(12):
+        print('senior')
+    else:
+        print('your not in highschool')
+
+main()
