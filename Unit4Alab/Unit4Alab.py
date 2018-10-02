5b7e6c08c719731e3205124e439724d496c242cb
Gabe-Lau <20laugabriel@bprep.org>
2018-10-02 09:18:48 -0700

    Unit 4 Lab

--- Unit4Alab/Unit4Alab.py
+++ Unit4Alab/Unit4Alab.py
@@ -1,8 +1,14 @@
-deVowel = input()
-print(deVowel)
+def main():
+   vowel = input('whats your string')
+   print(vowel)
+   print(deVowel(vowel))
 
-def deVowel():
-    vowel = ('a','e','i','o','u','y')
-
-
-
+def deVowel(vowel):
+    noVowel = ''
+    for x in vowel:
+        if(x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'y'):
+         y = 82
+        else:
+            noVowel = noVowel + x
+    return (noVowel)
+main()
