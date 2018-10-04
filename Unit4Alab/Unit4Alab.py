bb618dd5ae49f4e22a98e260fc9fa270f8f0cd66
Gabe-Lau <20laugabriel@bprep.org>
2018-10-04 09:00:28 -0700

    Unit 4A Lab

--- Unit4Alab/Unit4Alab.py
+++ Unit4Alab/Unit4Alab.py
@@ -6,8 +6,8 @@ def main():
 def deVowel(vowel):
     noVowel = ''
     for x in vowel:
-        if(x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'y'):
-         y = 82
+        if(x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'y' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U' or x == 'Y'):
+         y = 82000
         else:
             noVowel = noVowel + x
     return (noVowel)
