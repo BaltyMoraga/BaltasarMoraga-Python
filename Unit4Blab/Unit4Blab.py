fa4ac722aebce6e4cfe907fcb07cbfe209209a66
Gabe-Lau <20laugabriel@bprep.org>
2018-10-11 08:14:00 -0700

    unit 4 B lab

--- Unit4Blab/Unit4Blab.py
+++ Unit4Blab/Unit4Blab.py
@@ -1,1 +1,15 @@
-input()
+for i in range(0,5):
+    print(i)
+    answer = (i+10)
+    skrt = (i*10)
+    input('pause')
+    print(str(i) + '+ 10 =' + str(answer))
+    print(str(i) + '* 10 =' + str(skrt))
+
+myList = [10,20,30,40,50]
+print(myList)
+
+for a in range(0, len(myList)):
+    list = []
+    myList[a] = myList[a] * 10
+    print(myList)
