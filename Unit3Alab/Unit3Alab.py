f7db649fc97acfb22223a446115cfef03f5c9003
Gabe-Lau <20laugabriel@bprep.org>
2018-10-15 08:37:12 -0700

    good

--- Unit3Alab/Unit3Alab.py
+++ Unit3Alab/Unit3Alab.py
@@ -1,40 +1,40 @@
-#def myFunc():
-    #print('sus')
-    #print('sauce')
-    #print('you are fine')
+def myFunc():
+    print('sus')
+    print('sauce')
+    print('you are fine')
 
-#print('starts here')
-#myFunc()
-
-
-#def howManyYearsAtSchool():
-    #myGrade = input('what grade are you in at school? - ')
-    #print('you have been in school for ' + str(myGrade) + ' years')
-
-#howManyYearsAtSchool()
+print('starts here')
+myFunc()
 
 
-#def homeBase():
-    #home = input('what city are you from? - ')
-    #schoolYears = input('how many years have you been in school? - ')
-    #print('you are from - ' + str(home))
-    #print('you are in grade - ' + str(schoolYears))
+def howManyYearsAtSchool():
+    myGrade = input('what grade are you in at school? - ')
+    print('you have been in school for ' + str(myGrade) + ' years')
 
-#print('starts here')
-#homeBase()
+howManyYearsAtSchool()
+
+
+def homeBase():
+    home = input('what city are you from? - ')
+    schoolYears = input('how many years have you been in school? - ')
+    print('you are from - ' + str(home))
+    print('you are in grade - ' + str(schoolYears))
+
+print('starts here')
+homeBase()
 
 
 from random import *
 
-#def randomNumber():
-    #x = input('what is my start value? - ')
-    #3y = input('what is my end value? - ')
-    #myNumber = randint(int(x), int(y))
-    #print(str(myNumber) + ' is a random number between - ')
-    #print(str(x) + ' and ' + str(y))
+def randomNumber():
+    x = input('what is my start value? - ')
+    3y = input('what is my end value? - ')
+    myNumber = randint(int(x), int(y))
+    print(str(myNumber) + ' is a random number between - ')
+    print(str(x) + ' and ' + str(y))
 
-#print('starts here')
-#randomNumber()
+print('starts here')
+randomNumber()
 
 
 
