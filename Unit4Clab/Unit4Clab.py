f06ce7e1da12ff06b510e62d8baa93009d6d44b7
Gabe-Lau <20laugabriel@bprep.org>
2018-10-15 08:35:38 -0700

    unit4Clab

--- Unit4Clab/Unit4Clab.py
+++ Unit4Clab/Unit4Clab.py
@@ -1,12 +1,15 @@
 def main():
     draw7()
+    input('pasue')
     starsAndStripes()
+    input('pause')
+    incTriangle()
 
 def draw7():
     string = ''
     for i in range(0,7):
         string = ''
-        for i in range(0,7):
+        for x in range(0,7):
             string += '*'
         print(string)
 
@@ -18,4 +21,11 @@ def starsAndStripes():
         for x in range(0,7):
             print('-', end = '')
         print('')
+
+def incTriangle():
+    for i in range(1,8):
+        for j in range(0,i):
+            print(i, end = '')
+        print()
+
 main()
