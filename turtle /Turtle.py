2c46d372d5c8fe891379b28d0e3c237886171499
Gabe-Lau <20laugabriel@bprep.org>
2018-11-13 08:29:27 -0800

    turtle

--- turtle /Turtle.py
+++ turtle /Turtle.py
@@ -1,7 +1,9 @@
 import turtle
 
-turtle.color('red')
-turtle.bgcolor('black')
+turtle.title('turtle drawing')
+turtle.color('yellow','blue')
+turtle.bgcolor ('black')
+
 def main():
     square()
     rectangle()
@@ -19,6 +21,7 @@ def square():
     turtle.penup()
     turtle.goto(0,100)
     turtle.pendown()
+    turtle.color('red')
     turtle.forward(75)
     turtle.left(90)
     turtle.forward(75)
@@ -29,9 +32,20 @@ def square():
     turtle.left(90)
 
 def rectangle():
-    turtle.penup
-    turtle.goto(-100,0)
-    turtle.pendown
+    turtle.penup()
+    turtle.goto(-150,0)
+    turtle.pendown()
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.penup()
+    turtle.goto(-150,100)
+    turtle.pendown()
     turtle.forward(100)
     turtle.left(90)
     turtle.forward(50)
