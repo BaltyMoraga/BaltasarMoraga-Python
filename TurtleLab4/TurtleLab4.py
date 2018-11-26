68bf55e32f20c19d7b8fbe8aafb77ddf54bc76eb
Gabe-Lau <20laugabriel@bprep.org>
2018-11-26 09:17:26 -0800

    T4

--- TurtleLab4/TurtleLab4.py
+++ TurtleLab4/TurtleLab4.py
@@ -4,16 +4,33 @@ turtle.setup(width=750, height=700)
 turtle.title('turtle drawing')
 turtle.bgcolor ('black')
 turtle.speed(50)
+colors = ['#FF0000','#FF7100','#FFC600','#3EFF00','#00FFE9','#0078FF','#AB00FF','#FF0095']
+turtle.color(colors)
 
 def main():
     drawing()
+    filled_arc()
     turtle.exitonclick()
 
+def filled_arc(radius,angle,color):
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.forward(90)
+    turtle.circle(radius,angle)
+    turtle.left(90)
+    turtle.forward(radius)
+    turtle.end_fill()
+    turtle.left(180-angle)
+
+angle = 360/len(colors)
+for color in colors:
+    filled_arc(100,angle,color)
+    turtle.left(angle)
+
 def drawing():
-    turtle.color('red')
-    for x in range(1000):
+    for x in range(8):
         turtle.forward(x)
-        turtle.left(150)
-        turtle.left(39)
+        turtle.circle(140)
+        turtle.left(45)
 
 main()
