f15a80a312b54072000a8dae38ad41467d1b74bd
Gabe-Lau <20laugabriel@bprep.org>
2018-12-10 09:17:03 -0800

    Unit 7A Lab

--- Unit7ALab/Unit7ALab.py
+++ Unit7ALab/Unit7ALab.py
@@ -4,17 +4,32 @@ class Pet():
     def __init__(self,pType,pName,pBreed):
         self.type = pType
         self.name = pName
-        self.bread = pBreed
+        self.breed = pBreed
 
-    def whatIsIt(self):
+    def getType(self):
+        return(self.type)
+    def getName(self):
+        return(self.name)
+    def getBreed(self):
+        return(self.breed)
 
+
+myPet1 = Pet('dog','corona','scotish terrier')
+print(myPet1.petType)
+print(myPet1.getType())
+print(myPet1.getName())
+print(myPet1.getBreed())
+
+myPet2 = Pet('cat','')
 class Cage():
     petType = 'caged pet'
 
     def __init__(self,cType,cDanger):
         self.type = cType
-        self.denger = cDanger
+        self.danger = cDanger
 
-    def whatDanger(self):
+    def getType(self):
+        return(self.type)
+    def getDanger(self):
+        return(self.danger)
 
-myPet1 = Pet('dog','corona''scotish terrier')
