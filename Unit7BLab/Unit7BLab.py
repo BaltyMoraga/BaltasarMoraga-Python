da7aa10f8372d36acded5a741a28e3788c47d821
Gabe-Lau <20laugabriel@bprep.org>
2019-01-07 09:18:34 -0800

    unit 7 B

--- Unit7BLab/Unit7BLab.py
+++ Unit7BLab/Unit7BLab.py
@@ -1,14 +1,32 @@
-class parent():
-    class pet():
-        petType = 'cage free pet'
+class pet():
+    petType = 'cage free pet'
 
-        def __init__(self,pName,pBreed):
-            self.name = pName
-            self.breed = pBreed
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
 
-        def getName(self):
-            return(self.name)
-        def getBreed(self):
-            return(self.breed)
+    def info(self):
+        return(self.name + ' ' + self.breed)
 
-    mypet = pet('corona','scotish terrier')
+mypet = pet('corona','scotish terrier')
+print(mypet.info())
+
+class child():
+    y = 1
+
+class dog(pet):
+    def __init__(self,name,breed):
+        pet.__init__(self,name,breed)
+
+    def info(self):
+        return('This is ' + self.name + ' a ' + self.breed)
+
+    def action(self):
+         self.activity = 'running'
+         return('This is a '+ self.petType + ' who is a ' + self.breed + ' named ' + self.name + ' who is currently ' + self.activity + '.')
+
+mypet1 = pet('Corona','Scotish Terrier')
+print(mypet1.info())
+
+mypet2 = dog('Corona','Scotish Terrier')
+print(mypet2.action())
